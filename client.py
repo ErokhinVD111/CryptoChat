a4b8fd88f9ea3cd52a7f7d1f4a9b24efe6a7f249
Vladimir <vovaerohin123@gmail.com>
2023-09-24 17:07:16 +0500

    Тест асинхронного клиента

--- client.py
+++ client.py
@@ -21,51 +21,48 @@ class Client:
         self._header_length: int = int(config.message["header_length"])
         self._user_name: str = input_user()
 
-    def send_message(self, message: str) -> bool:
-        enc_message = message.encode('utf-8')
-        message_header: bytes = f"{len(enc_message):<{self._header_length}}".encode('utf-8')
-        if message:
-            self._socket_writer.write(message_header + enc_message)
+    async def _send_message(self) -> bool:
+        new_message = await asyncio.to_thread(input, f'{self._user_name} > ')
+        enc_message = new_message.encode('utf-8')
+        enc_message_header: bytes = f"{len(enc_message):<{self._header_length}}".encode('utf-8')
+        if new_message:
+            self._socket_writer.write(enc_message_header + enc_message)
+            await self._socket_writer.drain()
             return True
         return False
 
-    def _send_self_username(self):
+    async def _send_self_username(self):
         enc_username: bytes = self._user_name.encode('utf-8')
-        enc_header_username: bytes = f"{len(enc_username):<{self._header_length}}".encode('utf-8')
-        self._client_socket.send(enc_header_username + enc_username)
+        enc_username_header: bytes = f"{len(enc_username):<{self._header_length}}".encode('utf-8')
+        self._socket_writer.write(enc_username_header + enc_username)
+        await self._socket_writer.drain()
 
-    def connect(self):
+    async def connect(self):
         self._socket_reader, self._socket_writer = await asyncio.open_connection(self._server_ip, self._server_port)
-        await self._send_self_username()
         await self._begin_chat()
-        await self._receive_message()
-        self._send_self_username()
-        self._begin_chat()
 
-    def _receive_message(self):
-
-        def decode_message(username_header: bytes):
+    async def _receive_messages(self):
+        async def decode_message(username_header: bytes):
             username_length = int(username_header.decode('utf-8').strip())
-            username = self._client_socket.recv(username_length).decode('utf-8')
-            message_header = self._client_socket.recv(self._header_length)
+            username = (await self._socket_reader.readexactly(username_length)).decode('utf-8')
+            message_header = await self._socket_reader.readexactly(self._header_length)
             message_length = int(message_header.decode('utf-8').strip())
-            message = self._client_socket.recv(message_length).decode('utf-8')
+            message = (await self._socket_reader.readexactly(message_length)).decode('utf-8')
             print(f'{username} > {message}')
 
-        while True:
-            username_header: bytes = self._client_socket.recv(self._header_length)
-            if not len(username_header):
-                print('Connection closed by the server')
-                sys.exit()
+        username_header: bytes = await self._socket_reader.readexactly(self._header_length)
+        if not len(username_header):
+            print('Connection closed by the server')
+            sys.exit()
 
-            decode_message(username_header)
+        await decode_message(username_header)
 
-    def _begin_chat(self):
+    async def _begin_chat(self):
+        await self._send_self_username()
         while True:
             try:
-                new_message = input(f'{self._user_name} > ')
-                self.send_message(new_message)
-                self._receive_message()
+                await self._send_message()
+                asyncio.create_task(self._receive_messages())
 
             except IOError as e:
                 if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
@@ -78,7 +75,11 @@ class Client:
                 sys.exit()
 
 
-if __name__ == '__main__':
+async def main():
     config: Configuration = get_config()
     client = Client(config)
-    client.connect()
+    await client.connect()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
